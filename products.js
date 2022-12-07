6951a754e4f96b3993cb8f85bd5478a24163d6f3
Manal <manallmahmood.github.io>
2022-12-07 16:21:35 +0700

    product

---
+++ products.js
@@ -0,0 +1,60 @@
+// define data in JSON array
+var products = [
+    {
+        name: "Samsung TV 50",
+        quantity: 1,
+        ppu: 15000
+    },
+    {
+        name: "Xiaomi Fan",
+        quantity: 2,
+        ppu: 500
+    },
+    {
+        name: "iPhone 14 Pro Max Ultra",
+        quantity: 1,
+        ppu: 50000
+    },
+    {
+        name: "Logitec Gaming Mouse",
+        quantity: 1,
+        ppu: 4000
+    }
+]
+
+function loadData() {
+    let elem = document.getElementById("myName")
+    elem.innerHTML = "Manal Mahmood"
+
+    let productList = document.getElementById("productList")
+    let sum = 0
+    for (let p in products) {
+        let row = document.createElement("tr")
+        let productName = document.createElement("td")
+        productName.innerHTML = products[p].name
+
+        let quantity = document.createElement("td")
+        quantity.innerHTML = products[p].quantity
+        quantity.classList.add("text-right")
+
+        let ppu = document.createElement("td")
+        ppu.innerHTML = products[p].ppu
+        ppu.classList.add("text-right")
+
+        let total = document.createElement("td")
+        total.innerHTML = products[p].ppu * products[p].quantity 
+        total.classList.add("text-right")
+        sum = sum + (products[p].ppu * products[p].quantity )
+
+        row.appendChild(productName)
+        row.appendChild(quantity)
+        row.appendChild(ppu)
+        row.appendChild(total)
+        productList.appendChild(row)
+    }
+
+    let gross = document.getElementById("gross")
+    gross.innerHTML = 99999
+
+
+}
