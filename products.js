9a6d1bcb86bd595781b13ee8e8fd486e9f29a5ab
Manal <manallmahmood.github.io>
2023-01-14 11:13:33 +0700

    Init

--- products.js
+++ products.js
@@ -21,13 +21,67 @@ var products = [
         ppu: 30
     }
 ]
+// This function will pick the value from the <selet>
+// and add to the table
+function addToCart() {
+
+    let productObj = {
+        name: $('#products').val(),
+        quantity: $('#qty').val(),
+        ppu: $('#ppu').val()
+    }
+    
+
+    // Clear existing items in the table
+    // let productList = document.getElementById("productList")
+    // for (let x = 0; x < products.length; x++) {
+    //     productList.deleteRow(1)
+    // }
+    $('#productBody').html("")
+
+    products.push(productObj)
+    loadData()
+}
+
+
+// TODO Should use product ID instead of name
+function deleteProduct(index) {
+    console.log("DELETE",index)
+    delete products[index]  // delete the element from array
+    $('#productBody').html("")
+    loadData()
+}
 
 function loadData() {
-    let elem = document.getElementById("myName")
-    elem.innerHTML = "Manal Mahmood"
+    let allRows = ""
+    let gross = 0
+    for (let p in products) {
+        let cellName = `<td><img class='icon' src='icon-delete.png' onclick='deleteProduct("${p}")'> ` + products[p].name + "</td>"
+        let cellQuantity = '<td class="text-right">' + products[p].quantity + "</td>"
+        let cellPPU = '<td class="text-right">' + products[p].ppu + "</td>"
+        let total = products[p].ppu * products[p].quantity
+        gross += total
+        let cellTotal = '<td class="text-right">' + total + "</td>"
+        let row = `<tr>${cellName}${cellQuantity}${cellPPU}${cellTotal}</tr>`
+        allRows += row
+    }
+    $('#productBody').html(allRows)
 
+    $("#gross").html(gross)
+
+    let vat = gross * 0.07
+    let net = gross + vat
+    $("#vat").html(vat.toFixed(2))
+    $("#net").html(net.toFixed(2))
+
+}
+
+function loadDataOld() {
+
+    // $('#productBody').html('<tr><td>xxx</td><td>xxx</td><td>xxx</td><td>xxx</td></tr>')
+    
     let productList = document.getElementById("productList")
-    let sum = 0
+    let gross = 0
     for (let p in products) {
         let row = document.createElement("tr")
         let productName = document.createElement("td")
@@ -42,9 +96,9 @@ function loadData() {
         ppu.classList.add("text-right")
 
         let total = document.createElement("td")
-        total.innerHTML = products[p].ppu * products[p].quantity 
+        total.innerHTML = products[p].ppu * products[p].quantity
         total.classList.add("text-right")
-        sum = sum + (products[p].ppu * products[p].quantity )
+        gross += products[p].ppu * products[p].quantity
 
         row.appendChild(productName)
         row.appendChild(quantity)
@@ -53,8 +107,13 @@ function loadData() {
         productList.appendChild(row)
     }
 
-    let gross = document.getElementById("gross")
-    gross.innerHTML = 99999
+    let grossElem = document.getElementById("gross")
+    grossElem.innerHTML = gross
 
+    let vat = gross * 0.07
+    let net = gross + vat
+    document.getElementById("vat").innerHTML = vat.toFixed(2)
+    document.getElementById("net").innerHTML = net.toFixed(2)
 
 }
+
