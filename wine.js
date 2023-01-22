c38e9761d89c4b2ec3091dc9057098a38d8f0609
Manal <manallmahmood.github.io>
2023-01-22 20:19:03 +0700

    Init

--- wine.js
+++ wine.js
@@ -1,44 +1,57 @@
-import * as React from 'react'
-
-const wineImage = {
-    width: '10rem'
-}
-
-export default function Page1() {
-
-    /**
-     * 1. Fetch from https://api.sampleapis.com/wines/reds
-     * 2. Trsnsform into JSX 
-     */
-
-    let items = []
+import * as React from 'react' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, ButtonGroup, Button, Row, Col } from 'react-bootstrap';
+import WineCard from '../components/WineCard';
+import { useLocalStorage } from 'react-use';
+const dummyPrice = 59
+function PosPage() {
     let [wineTitles, setWineTitles] = React.useState([])
-    // Run once after the page finished loading
-    React.useEffect(async() => {
-        // This function will be run once
-        // Fetch from https://api.sampleapis.com/wines/reds
-        let res = await fetch('https://api.sampleapis.com/wines/reds')
-        let wines = await res.json()
-
-        for(let i = 0; i<wines.length; i++) {
-            console.log(wines[i].winery)
-            items.push(<li>
-                <div style={{width:"40rem", overflow: "hidden"}}>
-                    <div style={{width:"11rem", float: "left"}}> <img style={wineImage} src={wines[i].image}/></div>
-                    <div><b>{wines[i].wine}</b> - {wines[i].winery}</div>
-                </div>
-            </li>)
-        }
-
-        setWineTitles(items)
-    }, [])
-    return (
-        <>
-            <h1>Wines</h1>
-
-            <ul>
+    let [subMenu, setSubMenu] = React.useState('reds')
+    let [cart, setCart] = useLocalStorage('cart', [])
+    const checkout = typeof window !== 'undefined' ? localStorage.clear() : null    
+    function addToCard(wine) {
+        cart.push(wine)
+        console.table(cart)
+        setCart([...cart])
+    }
+    React.useEffect(() => {
+        let items = []
+        fetch(`https://api.sampleapis.com/wines/${subMenu}`)
+            .then(res => res.json())
+            .then((wines) => {
+                for (let i = 0; i < wines.length; i++) {
+                    items.push(
+                        <WineCard                            key={i}
+                            image={wines[i].image}
+                            title={wines[i].wine}
+                            price={dummyPrice}
+                            handleClick={() => { addToCard(wines[i]) }}
+                        />                    )                }
+                setWineTitles(items)
+            })
+    }, [subMenu])
+return   <Container>
+        <h1>Wine Shop</h1>
+        <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary" onClick={() => { setSubMenu('reds') }}>Red Wine</Button>
+        <Button variant="secondary" onClick={() => { setSubMenu('whites') }}>Whites Wine</Button>
+        </ButtonGroup>
+        <Row>
+            <Col>
+            <Row>
                 {wineTitles}
-            </ul>
-        </>
-    )
-}
+                </Row>
+                </Col>
+                <Col sm={3}>
+                <h2>Cart </h2>
+                {cart.map((item, i) => {
+                        return <Row key={i}>
+                            <Col>{item.winery}</Col>
+                            <Col>{dummyPrice}</Col></Row> })}                
+                <Row>
+                    Total: {cart.length * dummyPrice} Baht                
+                    </Row>
+                    </Col>
+                    </Row>
+                    </Container >}
+                    export default PosPage
